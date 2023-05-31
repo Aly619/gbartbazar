4bdf6e54caf08d56d1cb290dde175d36855be362
Muhammad Ali <m.mask619@gmail.com>
2023-05-31 13:22:46 +0500

    forget

--- src/Pages/ForgetPasswordPage/ForgetPassword.js
+++ src/Pages/ForgetPasswordPage/ForgetPassword.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import '../ForgetPasswordPage/ForgetPassword.css'
 
+import { useNavigate } from 'react-router-dom'
+
 const ForgetPassword = () => {
+  const navigate = useNavigate()
   return (
     <>
       <section className="forget-password">
         <div className="forgetpassword">
           <h1>Enter Email Address</h1>
           <input type="email" placeholder='Enter your E-mail' />
-          <p>Back to sign in</p>
+          <p onClick={()=> navigate(-1)} >Back to sign in</p>
           <button>Send</button>
 
         </div>
