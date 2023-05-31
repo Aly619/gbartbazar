e67db076273b4948ba33f8ca2fd53de2667dc414
Muhammad Ali <m.mask619@gmail.com>
2023-05-31 12:41:24 +0500

    rewiew page

--- src/Components/LandingCard/LandingCard.js
+++ src/Components/LandingCard/LandingCard.js
@@ -2,24 +2,46 @@ import React from 'react'
 import '../LandingCard/LandingCard.css'
 import { Link } from 'react-router-dom'
 
-const landingcard = ({landingimg, landingheader, profilepic, username, userdate}) => {
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faStar} from '@fortawesome/free-solid-svg-icons'
+
+const landingcard = ({ landingimg, landingheader, profilepic, username, userdate }) => {
   return (
     <>
-    <div className="landing-card">
-      <Link to='/post'>
-      <img className='landing-card-img' src={landingimg} alt="" />
-      </Link>
-        
+      <div className="landing-card">
+        <Link to='/post'>
+          <img className='landing-card-img' src={landingimg} alt="" />
+        </Link>
+        <div className="review-card-stars">
+          <input type="radio" name="rate" id="rate-5" />
+          <FontAwesomeIcon for="rate-5" className='fastar' icon={faStar} />
+          <input type="radio" name="rate" id="rate-4" />
+          <FontAwesomeIcon for="rate-4" className='fastar' icon={faStar} />
+          <input type="radio" name="rate" id="rate-3" />
+          <FontAwesomeIcon for="rate-3" className='fastar' icon={faStar} />
+          <input type="radio" name="rate" id="rate-2" />
+          <FontAwesomeIcon for="rate-2" className='fastar' icon={faStar} />
+          <input type="radio" name="rate" id="rate-1" />
+          <FontAwesomeIcon for="rate-1" className='fastar' icon={faStar} />
+
+        </div>
+
+
         <h1 className="landing-header">{landingheader}</h1>
 
         <Link to={'/profile'}><div className="profile-link">
-            <img className='landing-card-proimg' src={profilepic} alt="" />
-            <p className="user-name">{username}</p>
-            <div className="usergap"></div>
-            <p className="user-date">{userdate}</p>
+          <img className='landing-card-proimg' src={profilepic} alt="" />
+          <p className="user-name">{username}</p>
+          <div className="usergap"></div>
+          <p className="user-date">{userdate}</p>
         </div></Link>
-        
-    </div>
+
+        <Link to={'/post'}><div className="reviewbtn">
+          <button>Review</button>
+        </div></Link>
+       
+
+      </div>
     </>
   )
 }
