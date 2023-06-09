f0604ce9f94d8b5bf958b3124a2a5e47651f5e9a
Hasnain49 <hasnain72.qureshi@gmail.com>
2023-06-09 16:50:33 +0500

    changes on landing cards have been done

--- src/Components/LandingCard/LandingCard.js
+++ src/Components/LandingCard/LandingCard.js
@@ -1,7 +1,10 @@
 import React from 'react'
 import '../LandingCard/LandingCard.css'
 import { Link } from 'react-router-dom'
-import ReactStars from 'react-stars'
+// import ReactStars from 'react-stars'
+
+import { HiOutlinePencil } from 'react-icons/hi'
+import { TbTrash } from 'react-icons/tb'
 
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 // import { faStar } from '@fortawesome/free-solid-svg-icons'
@@ -10,14 +13,30 @@ const landingcard = ({ landingimg, landingheader, profilepic, username, userdate
   return (
     <>
       <div className="landing-card">
-        <Link to='/post'>
-          <div className="landing-card-image">
-            <img className='landing-card-img' src={landingimg} alt="" />
-          </div>
+        <div className="landing-card-top">
+          <div className="card-edit-del">
+            <div className="edit-del-circle">
+              <Link to={'/editpostproduct'}>
+                <HiOutlinePencil className='landing-card-icon' />
+              </Link>
+            </div>
+            <div className="edit-del-circle">
+              <Link>
+                <TbTrash className='landing-card-icon' />
+              </Link>
 
-        </Link>
-        <div className="review-card-stars">
-          {/* <input type="radio" name="rate" id="rate-5" />
+            </div>
+          </div>
+          <Link to='/post'>
+            <div className="landing-card-image">
+              <img className='landing-card-img' src={landingimg} alt="" />
+            </div>
+
+          </Link>
+        </div>
+
+        {/* <div className="review-card-stars">
+          <input type="radio" name="rate" id="rate-5" />
           <FontAwesomeIcon for="rate-5" className='fastar' icon={faStar} />
           <input type="radio" name="rate" id="rate-4" />
           <FontAwesomeIcon for="rate-4" className='fastar' icon={faStar} />
@@ -26,21 +45,22 @@ const landingcard = ({ landingimg, landingheader, profilepic, username, userdate
           <input type="radio" name="rate" id="rate-2" />
           <FontAwesomeIcon for="rate-2" className='fastar' icon={faStar} />
           <input type="radio" name="rate" id="rate-1" />
-          <FontAwesomeIcon for="rate-1" className='fastar' icon={faStar} /> */}
+          <FontAwesomeIcon for="rate-1" className='fastar' icon={faStar} />
 
           <ReactStars count={5} size={24} color1={'#ffd700'} />
 
-        </div>
+        </div> */}
 
 
         <h1 className="landing-header">{landingheader}</h1>
 
-        <Link to={'/profile'}><div className="profile-link">
-          <img className='landing-card-proimg' src={profilepic} alt="" />
-          <p className="user-name">{username}</p>
-          <div className="usergap"></div>
-          <p className="user-date">{userdate}</p>
-        </div></Link>
+        <Link to={'/profile'}>
+          <div className="profile-link">
+            <img className='landing-card-proimg' src={profilepic} alt="" />
+            <p className="user-name">{username}</p>
+            <div className="usergap"></div>
+            <p className="user-date">{userdate}</p>
+          </div></Link>
 
         <Link to={'/post'}><div className="reviewbtn">
           {/* <button>Review</button> */}
